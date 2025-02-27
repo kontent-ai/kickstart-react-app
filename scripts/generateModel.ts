3d5d0c8a83c92a8a304c680f4c15401cc0de980f
Ivan Kiral <kiralivan@gmail.com>
2025-02-27 12:45:38 +0100

    add scripts and update packages

---
+++ scripts/generateModel.ts
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv';
+import {generateDeliveryModelsAsync, generateEnvironmentModelsAsync} from "@kontent-ai/model-generator";
+
+
+dotenv.config();
+
+const {VITE_ENVIRONMENT_ID, VITE_MANAGEMENT_API_KEY} = process.env;
+
+if(!VITE_ENVIRONMENT_ID){
+  throw new Error("VITE_ENVIRONMENT_ID cannot be empty!");
+}
+
+if(!VITE_MANAGEMENT_API_KEY){
+  throw new Error("VITE_MAPI_API_KEY cannot be empty!");
+}
+
+await generateDeliveryModelsAsync(
+  {
+    environmentId: VITE_ENVIRONMENT_ID,
+    apiKey: VITE_MANAGEMENT_API_KEY,
+    addTimestamp: false,
+    createFiles: true,
+    outputDir: "./src/model",
+    moduleFileExtension: "ts",
+    fileResolvers: {
+      taxonomy: "camelCase",
+      contentType: "camelCase",
+      snippet: "camelCase"
+    },
+    formatOptions: {
+      printWidth: 120,
+      tabWidth: 2,
+      useTabs: false,
+      trailingComma: "all",
+      parser: "typescript"
+    }
+  }
+);
+
+await generateEnvironmentModelsAsync({
+  apiKey: VITE_MANAGEMENT_API_KEY,
+  environmentId: VITE_ENVIRONMENT_ID,
+  moduleFileExtension: "ts",
+  entities: ["languages", "taxonomies", "contentTypes", "snippets", "webhooks", "collections", "workflows", "assetFolders", "spaces"],
+  formatOptions: {
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: "all",
+    parser: "typescript"
+  },
+  createFiles: true,
+  outputDir: "./src/model/project",
+  addTimestamp: false,
+});
