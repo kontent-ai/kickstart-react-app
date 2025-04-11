e3e3c84f9fde8f88a0333691153d270b2ca10707
Ivan Kiral <kiralivan@gmail.com>
2025-04-11 16:47:26 +0200

    add center position co callToAction

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -22,10 +22,19 @@ const CallToAction: React.FC<CallToActionProps> = ({
   imagePosition = "left",
   style = "default",
 }) => {
+  const calculateLayout = (imagePosition: "left" | "right" | "center") => {
+    if (imagePosition === "left") {
+      return "xl:flex-row";
+    } else if (imagePosition === "right") {
+      return "xl:flex-row-reverse";
+    }
+    return "";
+  };
+
   return (
     <div
-      className={`${style === "burgundy" ? "burgundy-theme" : ""} flex flex-col xl:h-[420px] ${
-        imagePosition === "right" ? "xl:flex-row-reverse" : "xl:flex-row"
+      className={`${style === "burgundy" ? "burgundy-theme" : ""} flex flex-col ${
+        calculateLayout(imagePosition)
       } items-center gap-16`}
     >
       <div className="rounded-lg w-[560px]">
@@ -38,8 +47,8 @@ const CallToAction: React.FC<CallToActionProps> = ({
         />
       </div>
 
-      <div className="flex xl:flex-1 flex-col gap-5">
-        <h2 className={`flex text-6xl font-bold text-heading-2-color`}>{title}</h2>
+      <div className={`flex xl:flex-1 flex-col gap-5 ${imagePosition === "center" ? "items-center" : ""}`}>
+        <h2 className={`flex w-fit text-6xl font-bold text-heading-2-color`}>{title}</h2>
 
         <p className={`flex text-xl text-body-color line-clamp-5`}>
           {description}
